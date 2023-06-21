10db21dae3387e2f163ea1ba297514ccee9351cc
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2023-06-21 10:59:33 +0530

    Update Shell Script Q12.sh

--- Shell Script Q12.sh
+++ Shell Script Q12.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# Get the number from the user
 echo "Enter a number:"
 read number
-
-# Print the multiplication table of the number up to 10 lines
-for i in $(seq 1 10); do
+for((i=1;i<=10;i++)) 
+do
   echo "$number * $i = $(($number * $i))"
 done
-
