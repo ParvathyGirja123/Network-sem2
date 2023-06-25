0555dfa5007feeba2b7bcc4f0c8bf1eb90a1e27c
Parvathy <117814878+ParvathyGirja123@users.noreply.github.com>
2023-06-25 21:21:33 +0530

    Update Shell Script Q9.sh

--- Shell Script Q9.sh
+++ Shell Script Q9.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
-# Read marks from the user
 echo "Enter the marks for the student:"
 read -p "Mark 1: " mark1
 read -p "Mark 2: " mark2
 read -p "Mark 3: " mark3
-
-# Calculate the average
 average=$(( ($mark1 + $mark2 + $mark3) / 3 ))
-
-# Display the average
 echo "Average: $average"
-
-# Determine the grade based on the average
 if (( average >= 90 )); then
   grade="S"
 elif (( average >= 80 )); then
@@ -24,7 +16,5 @@ elif (( average >= 40 )); then
 else
   grade="F"
 fi
-
-# Display the grade
 echo "Grade: $grade"
 
